f4b8729b0a5bccbe3c03ce21ea5910058bcaa2c6
George Kourletakis <geokourl@gmail.com>
2021-04-20 15:45:23 +0300

    assignment2 event binding

--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { Assignment2Component } from './assignment2/assignment2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Assignment2Component
   ],
   imports: [
     BrowserModule,
